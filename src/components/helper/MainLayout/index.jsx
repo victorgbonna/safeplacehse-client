fd63972a73501d0fce4347f1105267c497525f57
victorgbonna <victorgbonna@gmail.com>
2026-05-06 18:29:22 +0100

    fixed ad 2

--- src/components/helper/MainLayout/index.jsx
+++ src/components/helper/MainLayout/index.jsx
@@ -1,12 +1,12 @@
 import { use, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Footer, Nav } from "@/components";
-import { Montserrat } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
 // import {Alan} from 'next/font/google'
 // import {Mon} from 'next/googlefont'
 // import Nav from "../Nav";
-// const monte = Montserrat({ subsets: ['latin'], weight:['400','500','600','700','800','900'] });
+const nunito = Nunito({ subsets: ['latin'], weight:['400','500','600','700','800','900'] });
 
 export default function MainLayout({ children, isLoggedIn}) {
   const [scrolledPast, setScrolledPast] = useState(false);
@@ -36,7 +36,7 @@ export default function MainLayout({ children, isLoggedIn}) {
     setActiveNav(router?.pathname)
   }, [router?.pathname])
   return (
-      <div className={" overflow-x-hidden max-w-full text-black bg-lightgreen w-screen relative bg-[#f8faed]"}> 
+      <div className={nunito.className+" overflow-x-hidden max-w-full text-black bg-lightgreen w-screen relative bg-[#f8faed]"}> 
         <Nav activeNav={activeNav} 
           scrolledPast={scrolledPast} 
           scrolledPastMobile={scrolledPastMobile}/>
